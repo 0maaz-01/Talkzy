afa823239effef7dafcc927dd7fc8948c6b47c8f
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-08 06:12:36 +0530

    Add files via upload

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+
+// We are naming the schema as User (singular), mongoose will automatically create a database named as users (plural) as it takes User from their and adds s in it.
+const User = mongoose.model("User", userSchema);
+
+export default User;
