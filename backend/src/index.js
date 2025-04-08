03351768178ef9d08653a79d0979ba6c3c384d78
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-08 06:10:35 +0530

    Add files via upload

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+dotenv.config()
+
+const app = express();
+
+// to extract the data from the body.
+
+const PORT = process.env.PORT
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true,
+    })
+);
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+app.listen(5001, () => {
+    console.log(`Server is running on PORT : ${PORT}`);
+    connectDB()
+})
